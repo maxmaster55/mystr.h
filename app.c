ded9b5c0ec466a58716c38bd78c229a5d2ffbd74
maxmaster55 <youssefalkashef000@gmail.com>
2023-02-24 18:37:56 +0200

    added the queue files

--- app.c
+++ app.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "stackpp.h"
-
-stackpp_t stack1;
-
 int main() {
-    stack_init(&stack1);
-    char x[] = "help";
-    stack_push(&stack1, &x);
-    printf("size = %d\n", stack1.size);
-    printf("top is: %s\n", stack1.stack[stack1.top_indx]);
-
-    char y[] = "help22";
-    stack_push(&stack1, &y);
-    printf("size = %d\n", stack1.size);
-    printf("top is: %s\n", stack1.stack[stack1.top_indx]);
 }
